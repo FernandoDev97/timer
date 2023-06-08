d2f86af337216e515da9bd90bfbfb3d365574a0d
Fernando Souza <l.fernando.dev@gmail.com>
2023-06-08 00:09:12 -0300

    finished styles history page

---
+++ src/pages/Hystory/index.tsx
@@ -0,0 +1,86 @@
+import { HystoryContainer, HystoryList } from './styles'
+
+export const History = () => {
+  return (
+    <HystoryContainer>
+      <h1>Meu histórico</h1>
+
+      <HystoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+          </tbody>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+          </tbody>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+          </tbody>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+          </tbody>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+          </tbody>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+          </tbody>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+          </tbody>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há 2 meses</td>
+              <td>Concluído</td>
+            </tr>
+          </tbody>
+        </table>
+      </HystoryList>
+    </HystoryContainer>
+  )
+}
